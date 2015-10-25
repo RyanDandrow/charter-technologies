db78f38fb3d3d73635041914bea2716a4d6acd19
Ryan Dandrow <ryandandrow@gmail.com>
2015-10-25 16:44:52 -0500

    Create admin user sessions controller / Add user settings to applicaiton controller

---
+++ app/controllers/admin_user_sessions_controller.rb
@@ -0,0 +1,32 @@
+class AdminUserSessionsController < ApplicationController
+    skip_before_filter :authorise, :only => [:new, :create]
+    before_filter :requires_no_session_user, :except => [:destroy]
+  
+    layout 'application_auth'
+  
+    def new
+      @admin_user_session = AdminUserSession.new
+    end
+  
+    def create
+      @admin_user_session = AdminUserSession.new params[:admin_user_session]
+      if @admin_user_session.save
+        redirect_back_or_default :controller => :pages, :action => :dashboard
+      else
+        render :action => :new
+      end
+    end
+  
+    def destroy
+      current_admin_user_session.destroy
+      redirect_back_or_default new_admin_user_session_url
+    end
+
+  private
+  
+    def requires_no_session_user
+      if current_user
+        redirect_to :controller => :pages, :action => :dashboard
+      end
+    end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,52 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  require 'authlogic'
+
+    layout 'application_main'
+   
+    helper_method :current_admin_user_session, :current_user
+    before_filter :authorise
+
+  private
+  
+    def authorise    
+      unless current_user
+        session[:return_to] = request.fullpath
+        redirect_to new_admin_user_session_url
+        return false
+      end
+    end
+  
+    def current_admin_user_session
+      return @current_admin_user_session if defined?(@current_admin_user_session)
+      @current_admin_user_session = AdminUserSession.find
+    end
+
+    def current_user
+      return @session_user if defined?(@session_user)
+      @session_user = current_admin_user_session && current_admin_user_session.admin_user
+    end
+  
+    def needs_admin
+      unless @session_user.is_admin?
+        redirect_to :controller => :pages, :action => :dashboard
+      end
+    end
+  
+    def needs_admin_or_current_user
+      unless @session_user.is_admin? || params[:id].to_i == @session_user.id
+        redirect_to :controller => :pages, :action => :dashboard
+      end
+    end
+    
+    def redirect_back_or_default(default)
+      redirect_to(session[:return_to] || default)
+      session[:return_to] = nil
+    end
+
+    def sort_order(default)
+      "#{(params[:c] || default.to_s).gsub(/[\s;'\"]/,'')} #{'ASC' if params[:d] == 'up'} #{'DESC' if params[:d] == 'down'}"
+    end
 end
