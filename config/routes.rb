5eb326d9e01dee38ca89098ab1530bd9071668d0
Ryan Dandrow <ryandandrow@gmail.com>
2015-10-27 02:06:09 -0500

    Create initial contacts form and index page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
         patch :update_password, :reset_password
       end
     end
+
+    resources :contacts
     
     resource :admin_user_session, :only => [:new, :create, :destroy]
     resource :password_reset, :only => [:create, :edit, :update]
