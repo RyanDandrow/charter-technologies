fc2bc2e56ff17691da42f16264692278a3f8e0c5
Ryan Dandrow <ryandandrow@gmail.com>
2015-10-28 01:03:58 -0500

    Set assets runtime errors alerts back to true

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,12 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.precompile += %w( bootstrap-overrides.css )
+Rails.application.config.assets.precompile += %w( custom.css )
+Rails.application.config.assets.precompile += %w( custom.js )
+Rails.application.config.assets.precompile += %w( comments.js.coffee )
+Rails.application.config.assets.precompile += %w( jquery.mask.min.js )
+Rails.application.config.assets.precompile += %w( login.css )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
