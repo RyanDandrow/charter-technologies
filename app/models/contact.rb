15fc52ba537c415287c2c91fc1b26ebc3c5a1b1f
Ryan Dandrow <ryandandrow@gmail.com>
2015-11-05 12:10:03 -0600

    Add validations for date_of_birth and tax_id for contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,6 +8,10 @@ class Contact < ActiveRecord::Base
 	validates :email, :allow_blank => true, format: { with: EMAIL_REGEX }
 	validates :spouse_email, :allow_blank => true, format: { with: EMAIL_REGEX }
 	validates :alt_email, :allow_blank => true, format: { with: EMAIL_REGEX }
+	validates_length_of :date_of_birth, :minimum => 10, :maximum => 10, :allow_blank => true, message: "is invalid"
+	validates_length_of :spouse_date_of_birth, :minimum => 10, :maximum => 10, :allow_blank => true, message: "is invalid"
+	validates_length_of :tax_id, :minimum => 10, :maximum => 10, :allow_blank => true, message: "ID is invalid"
+	
 
 	acts_as_commentable
 
@@ -28,6 +32,8 @@ class Contact < ActiveRecord::Base
 		  self.name = self.name.capitalize if self.name.present?
 		  self.middle_name = self.middle_name.capitalize if self.middle_name.present?
 		  self.last_name = self.last_name.capitalize if self.last_name.present?
+		  self.goes_by = self.goes_by.capitalize if self.goes_by.present?
+		  self.spouse_goes_by = self.spouse_goes_by.capitalize if self.spouse_goes_by.present?
 		end
 
 end
