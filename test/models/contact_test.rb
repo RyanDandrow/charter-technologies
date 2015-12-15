d069ac7835d3e255f8048b1d39198818b45db17d
Ryan Dandrow <ryandandrow@gmail.com>
2015-12-15 14:42:06 -0600

    Create additional tests for Contact model

--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -32,6 +32,13 @@ class ContactTest < ActiveSupport::TestCase
 		assert_not duplicate_contact.valid?
   end
 
+	test "Spouse email addresses should be unique" do
+		duplicate_contact = @contact.dup
+		duplicate_contact.spouse_email = @contact.spouse_email
+		@contact.save
+		assert_not duplicate_contact.valid?
+  end
+
 	test "cell phone should be unique" do
 		duplicate_contact = @contact.dup
 		duplicate_contact.cell_phone = @contact.cell_phone
@@ -41,7 +48,7 @@ class ContactTest < ActiveSupport::TestCase
 
 	test "home phone should be unique" do
 		duplicate_contact = @contact.dup
-		duplicate_contact.cell_phone = @contact.home_phone
+		duplicate_contact.home_phone = @contact.home_phone
 		@contact.save
 		assert_not duplicate_contact.valid?
   end
@@ -53,6 +60,36 @@ class ContactTest < ActiveSupport::TestCase
     		@contact.email = invalid_address
     		assert_not @contact.valid?, "#{invalid_address.inspect} should be invalid"
   	end
+	end
+
+	test "cell phone should reject invalid number" do
+  	invalid_number = %w[609-32-6771 60-000-4444 609-312-000]
+  	invalid_number.each do |invalid_number|
+    		@contact.cell_phone = invalid_number
+    		assert_not @contact.valid?, "#{invalid_number.inspect} should be invalid"
+  	end
+	end
+
+	test "home phone should reject invalid number" do
+  	invalid_number = %w[609-32-6771 60-000-4444 609-312-000]
+  	invalid_number.each do |invalid_number|
+    		@contact.home_phone = invalid_number
+    		assert_not @contact.valid?, "#{invalid_number.inspect} should be invalid"
+  	end
+	end
+
+	test "SF ID should be unique" do
+		duplicate_contact = @contact.dup
+		duplicate_contact.sf_id = @contact.sf_id
+		@contact.save
+		assert_not duplicate_contact.valid?
+	end
+
+	test "Tax ID should be unique" do
+		duplicate_contact = @contact.dup
+		duplicate_contact.tax_id = @contact.tax_id
+		@contact.save
+		assert_not duplicate_contact.valid?
 	end
 
 end
