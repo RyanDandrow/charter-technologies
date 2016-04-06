2e49236970318876edb92cfea45beef41bdd09c0
Ryan Dandrow <ryandandrow@gmail.com>
2016-04-05 22:01:10 -0500

    Add dropdown for selecting optional number of contacts per page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,14 +3,15 @@ class ContactsController < ApplicationController
 	  before_action :set_contact, only: [:show, :update, :destroy]
 
 	def index
-		@contacts = Contact.order(sort_order(:name)).paginate(:page => params[:page], :per_page => 10)
+    @per_page = params[:per_page] || Contact.per_page || 20
+		@contacts = Contact.order(sort_order(:name)).paginate(:page => params[:page], :per_page => @per_page)
 
     @search = Contact.search(params[:q])
-    @contacts = @search.result.order(sort_order(:name)).paginate(:page => params[:page], :per_page => 10)
+    @contacts = @search.result.order(sort_order(:name)).paginate(:page => params[:page], :per_page => @per_page)
 
     @advanced_search = Contact.search(params[:q])
     @advanced_search.build_condition if @advanced_search.conditions.empty?
-    @contacts = @advanced_search.result.order(sort_order(:name)).paginate(:page => params[:page], :per_page => 10)
+    @contacts = @advanced_search.result.order(sort_order(:name)).paginate(:page => params[:page], :per_page => @per_page)
 	end
 
 	def show
