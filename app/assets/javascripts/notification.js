aee2da764f95c48a0f99272e5e67f81ccfa1c4da
Ryan Dandrow <ryandandrow@gmail.com>
2016-04-07 10:15:03 -0500

    Update autosave success notification for contact form

--- app/assets/javascripts/notification.js
+++ app/assets/javascripts/notification.js
@@ -2,7 +2,7 @@ function notify(message, level) {
   if (level === undefined) level = "info";
   // display a notification under the page header
   $('<div class="alert alert-' + level + '">' + message + '</div>').
-    appendTo('.page-header').
+    appendTo('.footer').
     delay(5000).
     fadeOut(500, function() {
       $(this).remove()
